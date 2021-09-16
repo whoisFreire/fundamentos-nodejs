d3c39fa987be5e67770d311a7105425b6085d225
Leonardo Freire <leonardofreire.russi@icloud.com>
2021-09-15 23:05:08 -0300

    chapter I: Criando a estrutura do projeto

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (request, response)=> {
+    return response.json({
+        message: "hello world!"
+    })
+})
+
 app.listen(3333,()=>console.log('running'))
